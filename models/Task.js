9a3d686c3eeb53404764047c2a17e66b42f26f74
adriarodr <158720918+adriarodr@users.noreply.github.com>
2026-06-22 21:31:29 -0400

    feat: Add User and Task models

---
+++ models/Task.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  dueDate: Date,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
