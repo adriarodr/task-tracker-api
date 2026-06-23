59e242a3c0bbecb0efc56aaa589cad91ae786406
adriarodr <158720918+adriarodr@users.noreply.github.com>
2026-06-23 17:56:00 -0400

    feat: Add user registration and login routes to authRoutes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-// Load the variables form the .env file
-require('dotenv').config();
-
-const connectDB = () => {
+const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (err) {
     console.error('Database connection error:', err);
     process.exit(1);
