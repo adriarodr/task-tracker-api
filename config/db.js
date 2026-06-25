ec3083cd15d026a70eeea07694ead54eef03ef8a
adriarodr <158720918+adriarodr@users.noreply.github.com>
2026-06-25 13:49:56 -0400

    feat: improve logging information when connecting to MongoDB

--- config/db.js
+++ config/db.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-  } catch (err) {
-    console.error('Database connection error:', err);
-    process.exit(1);
-  }
+const connectDB = () => {
+  // Initial connection to MongoDB
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .catch((err) => console.log('Database connection error:', err));
+
+  // Connection events for when something happens after initial connection
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+  });
+
+  mongoose.connection.on('reconnected', () => {
+    console.log('Reconnected to MongoDB');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.log('Database connection error: ', err);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('Database disconnected');
+  });
 };
 
 module.exports = connectDB;
